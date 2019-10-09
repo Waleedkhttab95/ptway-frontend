cfef2ceb88e091dd92feb0e2f55030e65c4d5d92
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2019-10-09 16:19:46 +0300

    delete unnecessary files

--- src/services/universitiesServices.js
+++
@@ -1,7 +0,0 @@
-import baseRequest from "../_core";
-
-const universityService = {
-    getUniversity: (id) => baseRequest.get(`/get/searchUniverstyById?id=${id}`),
-  };
-
-  export default universityService;
