8b2adc8963451c3e2d895a497ff23ddb914ec75d
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2019-12-03 14:46:11 +0200

    add new features and fix bugs

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -6,7 +6,7 @@ import Contracts from '../Contracts';
 import Partners from '../Partners';
 import CommonQuestions from '../CommonQuestions';
 import ContactUs from '../Contact';
-import cloud2 from '../../images/Cloud2.png';
+// import cloud2 from '../../images/Cloud2.png';
 import Footer from '../Footer';
 const LandingPage = () => {
   return (
@@ -15,14 +15,14 @@ const LandingPage = () => {
       <HowItWorks />
       <JobsTypes />
       <Contracts />
-      <div style={{ position: 'relative', height: '1060px' }}>
+      {/* <div style={{ position: 'relative', height: '1060px' }}>
         <img src={cloud2} alt="cloud2" />
-        <div style={{ position: 'absolute', right: '155px' }}>
-          <Partners />
-          <CommonQuestions />
-        </div>
-      </div>
-      <br></br>
+        <div style={{ position: 'absolute', right: '155px' }}> */}
+      <Partners />
+      <CommonQuestions />
+      {/* </div>
+      </div> */}
+
       <ContactUs />
       <Footer />
     </React.Fragment>
