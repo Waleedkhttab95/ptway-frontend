81dc42c2f901906d666a7c5f233ca5b80d2176e2
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2019-12-23 13:36:32 +0200

    translate comQuestion section

--- src/components/CommonQuestions/index.js
+++ src/components/CommonQuestions/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Collapse } from 'antd';
 import './commonQ.scss';
+import { withTranslation } from 'react-i18next';
 
 const { Panel } = Collapse;
 const text = (
@@ -11,45 +12,46 @@ const text = (
     للأحرف عوضاً عن استخدام هنا يوجد محتوى نصي
   </p>
 );
-const CommonQuestions = () => {
+const CommonQuestions = props => {
+  const { i18n } = props;
   return (
     <Row>
       <div className="com-questions">
-        <h3 className="com-title">الأسئلة الأكثر شيوعاً</h3>
+        <h3 className="com-title">{i18n.t('home.comQuestions')} </h3>
         <div className="questions">
           <div className="question">
             <Collapse bordered={false} defaultActiveKey={['1']}>
               <Panel
                 className="qus-name"
-                header="ماهي الخطوات الأولية لتسجيل شركة جديدة في الموقع؟"
+                header={i18n.t('home.coQuestions.question1')}
                 key="1"
               >
                 {text}
               </Panel>
               <Panel
                 className="qus-name"
-                header="ماهي الخطوات الأولية لتسجيل شركة جديدة في الموقع؟"
+                header={i18n.t('home.coQuestions.question2')}
                 key="2"
               >
                 {text}
               </Panel>
               <Panel
                 className="qus-name"
-                header="ما هي اجراءات الاتفاق مع العميل وكيف تتم الاتفاقية وماهي بنود الحماية؟"
+                header={i18n.t('home.coQuestions.question3')}
                 key="3"
               >
                 {text}
               </Panel>
               <Panel
                 className="qus-name"
-                header="ماهي الخطوات الأولية لتسجيل شركة جديدة في الموقع؟"
+                header={i18n.t('home.coQuestions.question4')}
                 key="4"
               >
                 {text}
               </Panel>
               <Panel
                 className="qus-name"
-                header="ما الخطوات التي يجب أن اقوم بها كموظف اريد التسجيل في الموقع؟"
+                header={i18n.t('home.coQuestions.question5')}
                 key="5"
               >
                 {text}
@@ -101,4 +103,4 @@ const CommonQuestions = () => {
   );
 };
 
-export default CommonQuestions;
+export default withTranslation()(CommonQuestions);
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Row, Col, Radio, Input } from 'antd';
 import './contact.scss';
 import Illustration_Digital from '../../images/Illustration_Digital.svg';
+import { withTranslation } from 'react-i18next';
 
 const { TextArea } = Input;
-const ContactUs = () => {
+const ContactUs = props => {
+  const { i18n } = props;
   return (
     <Row className="contact-container">
-      <h3 className="contact-title">تواصل معنا </h3>
+      <h3 className="contact-title">{i18n.t('home.contact')} </h3>
       <div className="contact-us">
         <Col md={12}>
           <img
@@ -19,25 +21,33 @@ const ContactUs = () => {
         </Col>
         <Col md={12}>
           <div className="element">
-            <h4 className="element-title">سبب التواصل معنا</h4>
+            <h4 className="element-title"> {i18n.t('home.contact-reason')}</h4>
             <Radio.Group value={'مساعدة مالية'} className="radio-options">
-              <Radio value={1}>مساعدة عامة</Radio>
-              <Radio value={2}>تبليغ عن مشكلة</Radio>
-              <Radio value={3}>تواصل إعلاني</Radio>
+              <Radio value={1}>{i18n.t('home.general-help')}</Radio>
+              <Radio value={2}> {i18n.t('home.contact-report')}</Radio>
+              <Radio value={3}>{i18n.t('home.contact-ads')}</Radio>
             </Radio.Group>
             <form className="contact-form">
-              <Input placeholder="الاسم الكامل" className="contact-input" />
               <Input
-                placeholder="البريد الالكتروني"
+                placeholder={i18n.t('home.contact-fullname')}
                 className="contact-input"
               />
-              <Input placeholder="عنوان الرسالة" className="contact-input" />
+              <Input
+                placeholder={i18n.t('home.contact-email')}
+                className="contact-input"
+              />
+              <Input
+                placeholder={i18n.t('home.contact-title')}
+                className="contact-input"
+              />
               <TextArea
                 rows={4}
                 className="contact-input"
-                placeholder="نص الرسالة"
+                placeholder={i18n.t('home.contact-message')}
               />
-              <button className="contact-btn">ارسل الرسالة</button>
+              <button className="contact-btn">
+                {i18n.t('home.contact-send-btn')}
+              </button>
             </form>
           </div>
         </Col>
@@ -46,4 +56,4 @@ const ContactUs = () => {
   );
 };
 
-export default ContactUs;
+export default withTranslation()(ContactUs);
