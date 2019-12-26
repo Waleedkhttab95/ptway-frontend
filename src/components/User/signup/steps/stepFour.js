e44d641abbd0bb3217d212d90c04ce225a4dd7a6
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2019-12-26 15:16:18 +0200

    complet user registration feature

--- src/components/User/signup/steps/stepFour.js
+++ src/components/User/signup/steps/stepFour.js
@@ -1,40 +1,55 @@
 import React from 'react';
 import { Input } from 'antd';
 
-class step4Form extends React.Component {
-  render() {
-    return (
-      <div className="steps-form">
-        <div className="form-content" style={{ padding: '30px 53px 0 0' }}>
-          {/* <img src={personalInfoIcon} /> */}
-          <span className="f-title">معلومات الحساب</span>
-          <span className="line"></span>
-          <div className="form-fields">
-            <div className="first-row">
-              <div className="elements">
-                <label className="info-label">البريد الالكتروني</label>
-                <Input className="name-text" />
-              </div>
-              <div className="elements">
-                <label className="info-label">تأكيد البريد الالكتروني</label>
-                <Input className="name-text" />
-              </div>
+const step4Form = props => {
+  const { handleChange } = props;
+  return (
+    <div className="steps-form">
+      <div className="form-content" style={{ padding: '30px 53px 0 0' }}>
+        {/* <img src={personalInfoIcon} /> */}
+        <span className="f-title">معلومات الحساب</span>
+        <span className="line"></span>
+        <div className="form-fields">
+          <div className="first-row">
+            <div className="elements">
+              <label className="info-label">البريد الالكتروني</label>
+              <Input
+                className="name-text"
+                name="email"
+                onChange={handleChange}
+              />
             </div>
-            <div className="first-row">
-              <div className="elements">
-                <label className="info-label">كلمة المرور</label>
-                <Input className="name-text" />
-              </div>
-              <div className="elements">
-                <label className="info-label">تأكيد كلمة المرور</label>
-                <Input className="name-text" />
-              </div>
+            <div className="elements">
+              <label className="info-label">تأكيد البريد الالكتروني</label>
+              <Input
+                className="name-text"
+                name="re-email"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="first-row">
+            <div className="elements">
+              <label className="info-label">كلمة المرور</label>
+              <Input
+                className="name-text"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="elements">
+              <label className="info-label">تأكيد كلمة المرور</label>
+              <Input
+                className="name-text"
+                name="re-password"
+                onChange={handleChange}
+              />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default step4Form;
--- src/components/User/signup/steps/stepThree.js
+++ src/components/User/signup/steps/stepThree.js
@@ -1,33 +1,57 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Input, Select } from 'antd';
 import personalInfoIcon from '../../../../images/personal-info-icon.svg';
+import _ from 'lodash';
 
-class step3Form extends React.Component {
-  render() {
-    return (
-      <div className="steps-form">
-        <div className="form-content" style={{ padding: '30px 53px 0 0' }}>
-          <img src={personalInfoIcon} />
-          <span className="f-title">معلومات شخصية</span>
-          <span className="line"></span>
-          <div className="form-fields">
-            <div className="first-row">
-              <div className="elements">
-                <label className="info-label">الاسم الأول</label>
-                <Input className="name-text" />
-              </div>
-              <div className="elements">
-                <label className="info-label">الاسم الأخير</label>
-                <Input className="name-text" />
-              </div>
+const { Option } = Select;
+
+const step3Form = props => {
+  const { handleChange, handleSelect, majors } = props;
+  return (
+    <div className="steps-form">
+      <div className="form-content" style={{ padding: '30px 53px 0 0' }}>
+        <img src={personalInfoIcon} />
+        <span className="f-title">معلومات شخصية</span>
+        <span className="line"></span>
+        <div className="form-fields">
+          <div className="first-row">
+            <div className="elements">
+              <label className="info-label">الاسم الأول</label>
+              <Input
+                className="name-text"
+                name="firstName"
+                onChange={handleChange}
+              />
             </div>
-            <label className="info-label">التخصص العام</label>
-            <Input className="major-text" />
+            <div className="elements">
+              <label className="info-label">الاسم الأخير</label>
+              <Input
+                className="name-text"
+                name="lastName"
+                onChange={handleChange}
+              />
+            </div>
           </div>
+          <label className="info-label">التخصص العام</label>
+          <Select
+            onChange={value => handleSelect(value, 'major')}
+            className="major-text"
+          >
+            {_.isArray(majors)
+              ? majors.map(elm => {
+                  return (
+                    <Option value={elm.id} key={elm.id}>
+                      {elm.value}
+                    </Option>
+                  );
+                })
+              : ''}
+          </Select>
+          {/* <Input  name="major" onChange={handleChange} /> */}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default step3Form;
