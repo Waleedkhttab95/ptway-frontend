f635de65ac912ecc0a918d644229d5aaf6c55de3
ptway <waleed.khattab@ptway.net>
2020-01-14 17:52:11 +0300

    add prod url

--- src/_core/config.js
+++ src/_core/config.js
@@ -2,11 +2,11 @@ const config = {};
 let baseUrl;
 config.getBasePublicUrl = () => {
   if (process.env.NODE_ENV === 'development') {
-    baseUrl = 'https://ptway.net/';
+    baseUrl = 'http://localhost:80/';
   } else if (process.env.NODE_ENV === 'production') {
     baseUrl = '/';
   } else {
-    baseUrl = 'http://localhost:80/';
+    baseUrl = 'https://ptway.net/';
   }
   // todo: delete the line below
   return baseUrl;
