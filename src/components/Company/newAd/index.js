f9749e76257a9f866eb8d6c181822b6b679ace20
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-02-16 15:01:52 +0200

    make postJob feature

--- src/components/Company/newAd/index.js
+++ src/components/Company/newAd/index.js
@@ -1,74 +1,240 @@
 import React from 'react';
 import './style.scss';
 import Footer from '../../Footer';
-import { Input, Select } from 'antd';
-// import Header from '../../Header';
+import { Input, Select, DatePicker } from 'antd';
+import Header from '../../Header';
+import projects from '../../../services/company/projects';
+import { loadState } from '../../../_core/localStorage';
+import statatisticsService from '../../../services/statisticsService';
+import cvServices from '../../../services/user/cv';
+import _ from 'lodash';
+import moment from 'moment';
+const { allCities, allCountries } = statatisticsService;
+const { getPersonalSkills } = cvServices;
+const { getProjects, addNewAd } = projects;
 const { TextArea } = Input;
 const { Option } = Select;
 
-const AddNewAd = props => {
-  return (
-    <React.Fragment>
-      {/* <Header /> */}
-      <div className="company-container">
-        <div className="new-ad-form">
-          <h2 className="new-ad-title">إضافة إعلان جديد</h2>
-          <p>
-            أخيراً قم بتعبئة تفاصيل الإعلان الوظيفي الذي ترغب بإضافته ضمن مشروع
-          </p>
-          <div className="new-ad-details">
-            <label>تفاصيل الإعلان الوظيفي</label>
-            <TextArea row={6} />
-            <label>المشروع الأساسي الذي سيندرج تحته الإعلان</label>
-            <Select className="project-selection selector">
-              <Option value="jack">Option 1</Option>
-            </Select>
-            <label>المسمى الوظيفي المطلوب</label>
-            <Input />
-            <div className="group-questions">
-              <div className="right-side">
-                <label>عدد ساعات العمل</label>
-                <Input />
-                <label>الجنس</label>
-                <Input />
-                <label>المستوى التعليمي</label>
-                <Input />
+class AddNewAd extends React.Component {
+  state = {
+    allProjects: ''
+  };
+  async componentDidMount() {
+    const contractId = this.props.match.params.id;
+    const allProjects = await getProjects();
+    const countries = await allCountries();
+    const cities = await allCities();
+    const pSkills = await getPersonalSkills();
+    this.setState({
+      contractId,
+      allProjects,
+      countries,
+      cities,
+      pSkills
+    });
+  }
+
+  handleSelectChange = (value, option) => {
+    this.setState({
+      ...this.state,
+      [option.props.name]: option.key
+    });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      ...this.state,
+      [name]: value
+    });
+  };
+
+  DateChange = date => {
+    this.setState({ date });
+  };
+
+  handlePersonalSkillsChange = (value, option) => {
+    const ids = option.map(elm => elm.key);
+    this.setState({
+      personalSkills: ids
+    });
+  };
+  postAd = async () => {
+    console.log('this.state.contractId', this.state.contractId);
+
+    const data = {
+      contract: this.state.contractId,
+      jobDetails: this.state.jobDetails,
+      project: this.state.project,
+      workDays: this.state.workDays,
+      workHours: this.state.workHours,
+      personalSkills: this.state.personalSkills,
+      date: moment(this.state.data).format(),
+      jobDescription: this.state.jobDescription,
+      jobTitle: this.state.jobTitle,
+      gender: this.state.gender,
+      salary: this.state.salary,
+      country: this.state.country,
+      city: this.state.city,
+      required_Number: this.state.required_Number
+    };
+    await addNewAd(data);
+    const { history } = this.props;
+    if (loadState().loggedIn) {
+      window.location.reload(false);
+    } else {
+      history.push('/company/login');
+    }
+  };
+
+  render() {
+    const { allProjects, countries, cities, pSkills } = this.state;
+    const { loggedIn } = loadState();
+    return (
+      <React.Fragment>
+        {loggedIn && <Header />}
+        <div className="company-container">
+          <div className="new-ad-form">
+            <h2 className="new-ad-title">إضافة إعلان جديد</h2>
+            <p>
+              أخيراً قم بتعبئة تفاصيل الإعلان الوظيفي الذي ترغب بإضافته ضمن
+              مشروع
+            </p>
+            <div className="new-ad-details">
+              <label>تفاصيل الإعلان الوظيفي</label>
+              <TextArea
+                row={6}
+                onChange={this.handleChange}
+                name="jobDetails"
+              />
+              <label>المشروع الأساسي الذي سيندرج تحته الإعلان</label>
+              <Select
+                className="project-selection selector"
+                onChange={this.handleSelectChange}
+              >
+                {_.isArray(allProjects.proj)
+                  ? allProjects.proj.map(elm => (
+                      <Option
+                        value={elm.projectName}
+                        key={elm._id}
+                        name="project"
+                      >
+                        {elm.projectName}
+                      </Option>
+                    ))
+                  : ''}
+              </Select>
+              <label>المسمى الوظيفي المطلوب</label>
+              <Input onChange={this.handleChange} name="jobTitle" />
+              <div className="group-questions">
+                <div className="right-side">
+                  <label>عدد ساعات العمل</label>
+                  <Input onChange={this.handleChange} name="workHours" />
+                  <label>الجنس</label>
+                  <Select
+                    className="input-field"
+                    onChange={this.handleSelectChange}
+                  >
+                    <Option name="gender" value="male" key="ذكر">
+                      ذكر{' '}
+                    </Option>
+                    <Option name="gender" value="female" key="أنثى">
+                      أنثى{' '}
+                    </Option>
+                  </Select>
+                  <label>الراتب</label>
+                  <Input onChange={this.handleChange} name="salary" />
+                </div>
+                <div className="left-side">
+                  <label>عدد أيام العمل</label>
+                  <Input onChange={this.handleChange} name="workDays" />
+                  <label>تاريخ بدء العمل</label>
+                  <DatePicker
+                    onChange={this.DateChange}
+                    className="input-field"
+                    placeholder=""
+                  />
+                  <label>العدد المطلوب</label>
+                  <Input onChange={this.handleChange} name="required_Number" />
+                </div>
               </div>
-              <div className="left-side">
-                <label>عدد أيام العمل</label>
-                <Input />
-                <label>تاريخ بدء العمل</label>
-                <Input />
-                <label>المهارات العامة</label>
-                <Input />
+              <label>وصف الوظيفي</label>
+              <TextArea
+                row={4}
+                onChange={this.handleChange}
+                name="jobDescription"
+              />
+              <label>المهارات الشخصية :</label>
+              <br />
+              <Select
+                className="input-field"
+                mode="multiple"
+                onChange={this.handlePersonalSkillsChange}
+              >
+                {_.isArray(pSkills)
+                  ? pSkills.map(elm => {
+                      return (
+                        <Option
+                          value={elm.skillName}
+                          key={elm._id}
+                          name="skills"
+                        >
+                          {elm.skillName}
+                        </Option>
+                      );
+                    })
+                  : ''}
+              </Select>
+              <div className="last-ad-group">
+                <div>
+                  <label className="sub-heading">الدولة</label>
+                  <Select
+                    className="input-field"
+                    onChange={this.handleSelectChange}
+                  >
+                    {_.isArray(countries)
+                      ? countries.map(elm => {
+                          return (
+                            <Option
+                              value={elm.value}
+                              key={elm.id}
+                              name="country"
+                            >
+                              {elm.value}
+                            </Option>
+                          );
+                        })
+                      : ''}
+                  </Select>
+                </div>
+                <div>
+                  <label className="sub-heading">المدينة</label>
+                  <Select
+                    className="input-field"
+                    onChange={this.handleSelectChange}
+                  >
+                    {_.isArray(cities)
+                      ? cities.map(elm => {
+                          return (
+                            <Option value={elm.value} key={elm.id} name="city">
+                              {elm.value}
+                            </Option>
+                          );
+                        })
+                      : ''}
+                  </Select>
+                </div>
               </div>
             </div>
-            <label>وصف الوظيفي</label>
-            <TextArea row={4} />
-            <label>الموقع</label>
-            <Input />
-            <div className="last-ad-group">
-              <div>
-                <label className="sub-heading">الدولة</label>
-                <Select className="selector"></Select>
-              </div>
-              <div>
-                <label className="sub-heading">المدينة</label>
-                <Select className="selector"></Select>
-              </div>
-            </div>
+            <button className="add-new-ad-btn" onClick={this.postAd}>
+              أضف الإعلان الوظيفي
+            </button>
           </div>
-          <button
-            className="add-new-ad-btn"
-            onClick={() => props.history.push('/company/login')}
-          >
-            أضف الإعلان الوظيفي
-          </button>
         </div>
-      </div>
-      <Footer />
-    </React.Fragment>
-  );
-};
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
 export default AddNewAd;
--- src/services/company/projects.js
+++ src/services/company/projects.js
@@ -6,7 +6,33 @@ const projects = {
       projectName: params.projectName,
       projectDescription: params.projectDescription
     }),
-  getCotracts: () => baseRequest.get('/getcontracts')
+  getCotracts: () => baseRequest.get('/getcontracts'),
+  getProjects: () => baseRequest.get('/getprojects'),
+  addNewAd: params => {
+    let personal_Skills;
+    if (params.personalSkills != null) {
+      for (var i = 0; i < params.personalSkills.length; i++) {
+        personal_Skills = params.personalSkills[i];
+      }
+    } else {
+      personal_Skills = params.personalSkills;
+    }
+    return baseRequest.post('/postjob', {
+      contract: params.contract,
+      project: params.project,
+      job_Name: params.jobTitle,
+      startDate: params.date,
+      country: params.country,
+      city: params.city,
+      work_hours: params.workHours,
+      descreption: params.jobDescription,
+      work_days: params.workDays,
+      salary: params.salary,
+      gender: params.gender,
+      personal_Skills,
+      required_Number: params.required_Number
+    });
+  }
 };
 
 export default projects;
