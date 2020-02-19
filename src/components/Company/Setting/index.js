32b6883f6d47f421c1a8afc95ee9c0924eba0c08
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-02-19 17:00:04 +0200

    fix sidebar bug in the setting page

--- src/components/Company/Setting/index.js
+++ src/components/Company/Setting/index.js
@@ -6,38 +6,60 @@ import CompanyInfo from '../CompanyInfo';
 import Tab1 from './Tab1';
 import Tab2 from './Tab2';
 import Tab3 from './Tab3';
+import { connect } from 'react-redux';
 
+import { companyInfo } from '../../../store/actions/company/home';
 import Footer from '../../Footer';
 
 const { TabPane } = Tabs;
 
-const CompanySetting = () => {
-  return (
-    <React.Fragment>
-      <Header />
-      <div className="company-container">
-        <div className="company-setting">
-          <Col md={6}>
-            <CompanyInfo />
-          </Col>
-          <Col md={18} className="right-side">
-            <Tabs type="card">
-              <TabPane tab="بروفايل الشركة" key="1">
-                <Tab1 />
-              </TabPane>
-              <TabPane tab="الحسابات الفرعية" key="2">
-                <Tab2 />
-              </TabPane>
-              <TabPane tab="الإعدادات العامة" key="3">
-                <Tab3 />
-              </TabPane>
-            </Tabs>
-            ,
-          </Col>
+class CompanySetting extends React.Component {
+  componentDidMount() {
+    const { getCompanyInfo } = this.props;
+    getCompanyInfo();
+  }
+
+  render() {
+    const { company } = this.props;
+    return (
+      <React.Fragment>
+        <Header />
+        <div className="company-container">
+          <div className="company-setting">
+            <Col md={6}>
+              <CompanyInfo {...company} />
+            </Col>
+            <Col md={18} className="right-side">
+              <Tabs type="card">
+                <TabPane tab="بروفايل الشركة" key="1">
+                  <Tab1 />
+                </TabPane>
+                <TabPane tab="الحسابات الفرعية" key="2">
+                  <Tab2 />
+                </TabPane>
+                <TabPane tab="الإعدادات العامة" key="3">
+                  <Tab3 />
+                </TabPane>
+              </Tabs>
+              ,
+            </Col>
+          </div>
         </div>
-      </div>
-      <Footer />
-    </React.Fragment>
-  );
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+const mapStateToProps = ({ companySection }) => {
+  return {
+    company: companySection
+  };
 };
-export default CompanySetting;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getCompanyInfo: () => dispatch(companyInfo())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompanySetting);
