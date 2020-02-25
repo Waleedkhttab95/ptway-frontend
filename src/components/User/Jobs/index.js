db25af38ca1b637003292688dc72de155b0008df
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-02-25 21:34:31 +0200

    add spinner in job offers page

--- src/components/User/Jobs/index.js
+++ src/components/User/Jobs/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Header from '../../Header';
 import Footer from '../../Footer';
 import './style.scss';
-import { Row, Col } from 'antd';
+import { Row, Col, Spin } from 'antd';
 import { connect } from 'react-redux';
 import { jobOffers, applyJob } from '../../../store/actions/user/jobOffers';
 import _ from 'lodash';
 import FilterAndSearch from '../../Filter';
 
 class Jobs extends React.Component {
+  state = {
+    loading: true
+  };
   async componentDidMount() {
     const { jobOffers } = this.props;
     await jobOffers();
+    this.setState({
+      loading: false
+    });
   }
 
   jobOffer = async id => {
@@ -39,72 +45,78 @@ class Jobs extends React.Component {
           <div className="user-jobs">
             <FilterAndSearch />
             <Row className="jobs-details">
-              {_.isArray(offers.jobOffers.result)
-                ? offers.jobOffers.result.map(elm => {
-                    return (
-                      <Col md={6} className="job-post" key={elm.jobAd._id}>
-                        <div className="post-header">
-                          {/* <img src="" alt="" className="post-img" /> */}
-                          <i
-                            className="fa fa-picture-o"
-                            aria-hidden="true"
-                            style={{
-                              fontSize: '45px',
-                              display: 'flex',
-                              alignItems: 'center'
-                            }}
-                          ></i>
-                          <div className="job-owner-info">
-                            <span className="job-owner-title">
-                              {elm.jobAd.job_Name}
-                            </span>
-                            <span className="job-owner-location">
-                              في مقر الشركة
-                            </span>
-                            {/* <span className="job-owner-mobile">
+              {_.isArray(offers.jobOffers.result) ? (
+                offers.jobOffers.result.map(elm => {
+                  return (
+                    <Col md={6} className="job-post" key={elm.jobAd._id}>
+                      <div className="post-header">
+                        {/* <img src="" alt="" className="post-img" /> */}
+                        <i
+                          className="fa fa-picture-o"
+                          aria-hidden="true"
+                          style={{
+                            fontSize: '45px',
+                            display: 'flex',
+                            alignItems: 'center'
+                          }}
+                        ></i>
+                        <div className="job-owner-info">
+                          <span className="job-owner-title">
+                            {elm.jobAd.job_Name}
+                          </span>
+                          <span className="job-owner-location">
+                            في مقر الشركة
+                          </span>
+                          {/* <span className="job-owner-mobile">
                               الرقم : 0002163477555
                             </span> */}
-                          </div>
                         </div>
-                        <div className="post-body">
-                          <span className="post-description">
-                            {elm.jobAd.descreption}
-                          </span>
-                          <div className="post-actions-btns">
-                            <div className="post-status">
-                              {elm.status ? (
-                                <span>تم التقدم للعمل</span>
-                              ) : (
-                                <React.Fragment>
-                                  <span>لم يتم التقدم</span>
-                                  <button
-                                    className="apply-job-btn"
-                                    onClick={() => this.applyJob(elm.jobAd._id)}
-                                  >
-                                    التقدم للعمل
-                                  </button>
-                                </React.Fragment>
-                              )}
-                            </div>
+                      </div>
+                      <div className="post-body">
+                        <span className="post-description">
+                          {elm.jobAd.descreption}
+                        </span>
+                        <div className="post-actions-btns">
+                          <div className="post-status">
+                            {elm.status ? (
+                              <span>تم التقدم للعمل</span>
+                            ) : (
+                              <React.Fragment>
+                                <span>لم يتم التقدم</span>
+                                <button
+                                  className="apply-job-btn"
+                                  onClick={() => this.applyJob(elm.jobAd._id)}
+                                >
+                                  التقدم للعمل
+                                </button>
+                              </React.Fragment>
+                            )}
+                          </div>
 
-                            <button
-                              className="details-btn"
-                              onClick={() =>
-                                this.props.history.push(
-                                  `/user/job/${elm.jobAd._id}`
-                                )
-                              }
-                            >
-                              التفاصيل
-                            </button>
-                          </div>
+                          <button
+                            className="details-btn"
+                            onClick={() =>
+                              this.props.history.push(
+                                `/user/job/${elm.jobAd._id}`
+                              )
+                            }
+                          >
+                            التفاصيل
+                          </button>
                         </div>
-                      </Col>
-                    );
-                  })
-                : ''}
+                      </div>
+                    </Col>
+                  );
+                })
+              ) : (
+                <div className="spinner-loading">
+                  <Spin size="large" />
+                </div>
+              )}
             </Row>
-            <button className="display-more">عرض المزيد</button>
+            {!this.state.loading && (
+              <button className="display-more">عرض المزيد</button>
+            )}
           </div>
         </div>
         <Footer />
