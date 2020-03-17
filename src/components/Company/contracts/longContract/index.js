6d6a28a3b4ac2493535d4cec1f4b252c93437aa7
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-03-17 22:56:13 +0200

    validate all the contract fields in all contracts types as needed

---
+++ src/components/Company/contracts/longContract/index.js
@@ -0,0 +1,404 @@
+import React from 'react';
+import '../style.scss';
+import Footer from '../../../Footer';
+import { Input, Select, DatePicker, Modal } from 'antd';
+import Header from '../../../Header';
+import projects from '../../../../services/company/projects';
+import { loadState } from '../../../../_core/localStorage';
+import statatisticsService from '../../../../services/statisticsService';
+import cvServices from '../../../../services/user/cv';
+import _ from 'lodash';
+import moment from 'moment';
+import { Formik } from 'formik';
+import validationSchema from './validation';
+const { allCities, allCountries } = statatisticsService;
+const { getPersonalSkills } = cvServices;
+const { getProjects, addNewAd } = projects;
+const { TextArea } = Input;
+const { Option } = Select;
+
+class AddNewAd extends React.Component {
+  state = {
+    allProjects: '',
+    SuccessMsg: false,
+    error: false
+  };
+  async componentDidMount() {
+    const contractId = this.props.match.params.id;
+    const allProjects = await getProjects();
+    const countries = await allCountries();
+    const cities = await allCities();
+    const pSkills = await getPersonalSkills();
+    this.setState({
+      contractId,
+      allProjects,
+      countries,
+      cities,
+      pSkills
+    });
+  }
+
+  handleSelectChange = (value, option) => {
+    this.setState({
+      ...this.state,
+      [option.props.name]: option.key
+    });
+  };
+
+  DateChange = date => {
+    this.setState({ date });
+  };
+
+  handlePersonalSkillsChange = (value, option) => {
+    const ids = option.map(elm => elm.key);
+    this.setState({
+      personalSkills: ids
+    });
+  };
+  postAd = async values => {
+    console.log('values', values);
+
+    console.log('this.state.contractId', this.state.contractId);
+    const { project, gender, personalSkills, date, country, city } = this.state;
+    if (!project || !personalSkills || !gender || !date || !country || !city) {
+      this.setState({
+        error: true
+      });
+    } else {
+      const data = {
+        contract: this.state.contractId,
+        jobDetails: values.jobDetails,
+        project: this.state.project,
+        workDays: values.workDays,
+        workHours: values.workHours,
+        personalSkills: this.state.personalSkills,
+        date: moment(this.state.data).format(),
+        jobDescription: values.jobDescription,
+        jobTitle: values.jobTitle,
+        gender: this.state.gender,
+        salary: values.salary,
+        country: this.state.country,
+        city: this.state.city,
+        required_Number: values.required_Number
+      };
+      await addNewAd(data);
+      const { history } = this.props;
+      if (loadState().loggedIn) {
+        this.setState({
+          SuccessMsg: true
+        });
+      } else {
+        history.push('/company/login');
+      }
+    }
+  };
+
+  render() {
+    const { allProjects, countries, cities, pSkills, error } = this.state;
+    const { loggedIn } = loadState();
+    console.log('------', this.state);
+
+    return (
+      <React.Fragment>
+        {loggedIn && <Header />}
+        <div className="company-container">
+          <Formik
+            initialValues={{
+              workDays: 0,
+              workHours: 0,
+              salary: 0,
+              project: ''
+            }}
+            validationSchema={validationSchema}
+            onSubmit={values => this.postAd(values)}
+          >
+            {({ handleSubmit, errors, handleChange, touched, handleBlur }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="new-ad-form">
+                  <h2 className="new-ad-title">إضافة إعلان جديد</h2>
+                  <p>
+                    أخيراً قم بتعبئة تفاصيل الإعلان الوظيفي الذي ترغب بإضافته
+                    ضمن مشروع
+                  </p>
+                  <div className="new-ad-details">
+                    <label>تفاصيل الإعلان الوظيفي</label>
+                    <TextArea
+                      row={6}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="jobDetails"
+                    />
+                    {errors.jobDetails && touched.jobDetails && (
+                      <span style={{ color: 'red', fontSize: '12px' }}>
+                        {error.jobDetails}
+                      </span>
+                    )}
+                    <br />
+                    <br />
+                    <label>المشروع الأساسي الذي سيندرج تحته الإعلان</label>
+                    <Select
+                      className="project-selection selector"
+                      onChange={this.handleSelectChange}
+                    >
+                      {_.isArray(allProjects.proj)
+                        ? allProjects.proj.map(elm => (
+                            <Option
+                              value={elm.projectName}
+                              key={elm._id}
+                              name="project"
+                            >
+                              {elm.projectName}
+                            </Option>
+                          ))
+                        : ''}
+                    </Select>
+                    {error && !this.state.project && (
+                      <span style={{ color: 'red', fontSize: '12px' }}>
+                        هذا الحقل مطلوب
+                      </span>
+                    )}
+                    <br />
+                    <br />
+                    <label>المسمى الوظيفي المطلوب</label>
+                    <Input
+                      onChange={handleChange}
+                      name="jobTitle"
+                      onBlur={handleBlur}
+                    />
+                    {errors.jobTitle && touched.jobTitle && (
+                      <span style={{ color: 'red', fontSize: '12px' }}>
+                        {errors.jobTitle}
+                      </span>
+                    )}
+                    <br />
+                    <br />
+                    <div className="group-questions">
+                      <div className="right-side">
+                        <label>عدد ساعات العمل</label>
+                        <Input
+                          onChange={handleChange}
+                          name="workHours"
+                          type="number"
+                          onBlur={handleBlur}
+                        />
+                        <br />
+                        {errors.workHours && touched.workHours && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            {errors.workHours}
+                          </span>
+                        )}
+                        <br />
+                        <label>الجنس</label>
+                        <Select
+                          className="input-field"
+                          onChange={this.handleSelectChange}
+                        >
+                          <Option name="gender" value="male" key="ذكر">
+                            ذكر{' '}
+                          </Option>
+                          <Option name="gender" value="female" key="أنثى">
+                            أنثى{' '}
+                          </Option>
+                        </Select>
+                        <br />
+                        {error && !this.state.gender && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            هذا الحقل مطلوب
+                          </span>
+                        )}
+                        <br />
+                        <label>الراتب</label>
+                        <Input
+                          onChange={handleChange}
+                          name="salary"
+                          type="number"
+                          onBlur={handleBlur}
+                        />
+                        <br />
+                        {errors.salary && touched.salary ? (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            {errors.salary}
+                          </span>
+                        ) : (
+                          ''
+                        )}
+                        <br />
+                      </div>
+                      <div className="left-side">
+                        <label>عدد أيام العمل</label>
+                        <Input
+                          onChange={handleChange}
+                          name="workDays"
+                          type="number"
+                          onBlur={handleBlur}
+                        />
+                        <br />
+                        {errors.workDays && touched.workDays && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            أيام العمل يجب ان تكون واحدة من هذه 2,3,4,5,6 الأشهر
+                          </span>
+                        )}
+                        <br />
+                        <label>تاريخ بدء العمل</label>
+                        <DatePicker
+                          onChange={this.DateChange}
+                          className="input-field"
+                          placeholder=""
+                        />
+                        <br />
+                        {error && !this.state.date && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            هذا الحقل مطلوب
+                          </span>
+                        )}
+                        <br />
+                        <label>العدد المطلوب</label>
+                        <Input
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          name="required_Number"
+                          type="number"
+                        />
+                        <br />
+                        {errors.required_Number && touched.required_Number && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            {errors.required_Number}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                    <br />
+                    <label>وصف الوظيفي</label>
+                    <TextArea
+                      row={4}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="jobDescription"
+                    />
+                    {errors.jobDescription && touched.jobDescription && (
+                      <span style={{ color: 'red', fontSize: '12px' }}>
+                        {errors.jobDescription}
+                      </span>
+                    )}
+                    <br />
+                    <label>المهارات الشخصية :</label>
+                    <br />
+                    <Select
+                      className="input-field"
+                      mode="multiple"
+                      onChange={this.handlePersonalSkillsChange}
+                    >
+                      {_.isArray(pSkills)
+                        ? pSkills.map(elm => {
+                            return (
+                              <Option
+                                value={elm.skillName}
+                                key={elm._id}
+                                name="skills"
+                              >
+                                {elm.skillName}
+                              </Option>
+                            );
+                          })
+                        : ''}
+                    </Select>
+                    <br />
+                    {error && !this.state.personalSkills && (
+                      <span style={{ color: 'red', fontSize: '12px' }}>
+                        هذا الحقل مطلوب
+                      </span>
+                    )}
+                    <br />
+                    <div className="last-ad-group">
+                      <div>
+                        <label className="sub-heading">الدولة</label>
+                        <Select
+                          className="input-field"
+                          onChange={this.handleSelectChange}
+                        >
+                          {_.isArray(countries)
+                            ? countries.map(elm => {
+                                return (
+                                  <Option
+                                    value={elm.value}
+                                    key={elm.id}
+                                    name="country"
+                                  >
+                                    {elm.value}
+                                  </Option>
+                                );
+                              })
+                            : ''}
+                        </Select>
+                        <br />
+                        {error && !this.state.country && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            هذا الحقل مطلوب
+                          </span>
+                        )}
+                        <br />
+                      </div>
+                      <div>
+                        <label className="sub-heading">المدينة</label>
+                        <Select
+                          className="input-field"
+                          onChange={this.handleSelectChange}
+                        >
+                          {_.isArray(cities)
+                            ? cities.map(elm => {
+                                return (
+                                  <Option
+                                    value={elm.value}
+                                    key={elm.id}
+                                    name="city"
+                                  >
+                                    {elm.value}
+                                  </Option>
+                                );
+                              })
+                            : ''}
+                        </Select>
+                        <br />
+                        {error && !this.state.city && (
+                          <span style={{ color: 'red', fontSize: '12px' }}>
+                            هذا الحقل مطلوب
+                          </span>
+                        )}
+                        <br />
+                      </div>
+                    </div>
+                  </div>
+                  <button className="add-new-ad-btn">
+                    أضف الإعلان الوظيفي
+                  </button>
+                  <Modal
+                    visible={this.state.SuccessMsg}
+                    closable={false}
+                    footer={false}
+                  >
+                    <div className="success-modal">
+                      <i
+                        className="fa fa-check-circle check-icon"
+                        aria-hidden="true"
+                      ></i>
+                      <h2>تمت الاضافة بنجاح</h2>
+                      <button
+                        onClick={() => this.props.history.push('/company/home')}
+                      >
+                        العودة للرئيسية
+                      </button>
+                    </div>
+                  </Modal>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </div>
+
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default AddNewAd;
---
+++ src/components/Company/contracts/shortContract/validation.js
@@ -0,0 +1,21 @@
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+  workDays: yup
+    .number()
+    .min(1, 'يجب ان يكون عدد أيام العمل بين 1 و 59')
+    .max(59, 'يجب ان يكون عدد أيام العمل بين 1 و 59')
+    .required('هذا الحقل مطلوب'),
+  workHours: yup
+    .number()
+    .min(1, 'يجب ان يكون عدد ساعات العمل بين 1 و 12')
+    .max(12, 'يجب ان يكون عدد ساعات العمل بين 1 و 12')
+    .required('هذا الحقل مطلوب'),
+  salary: yup.number().min(100, 'الحد اليومي الأدنى هو 100'),
+  jobDescription: yup.string().required('هذا الحقل مطلوب'),
+  jobTitle: yup.string().required('هذا الحقل مطلوب'),
+  required_Number: yup.string().required('هذا الحقل مطلوب'),
+  jobDetails: yup.string().required('هذا الحقل مطلوب')
+});
+
+export default validationSchema;
