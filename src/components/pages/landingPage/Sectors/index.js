e27204bb39a70a9a683ce1d699f40f09ebc65111
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-04-02 22:34:53 +0300

    build sector carousel

--- src/components/pages/landingPage/Sectors/index.js
+++ src/components/pages/landingPage/Sectors/index.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Row, Carousel } from 'antd';
+import { Row } from 'antd';
 import './style.scss';
+import Carousel from './Carousel';
 import { withTranslation } from 'react-i18next';
 
-const JobsTypes = props => {
+const Sectors = props => {
   const { i18n } = props;
   return (
-    <Row className="sectors">
-      <h2 className="title">القطاعات</h2>
-      <p className="desc">
-        نغطي معظم مدن المملكة، ونتوسع يومياً. نعمل في قطاعات ومجالات مختلفة في
-        السوق السعودي، كالضيافة، قطاع التجزئة، القطاع الصحي والمزيد...
-      </p>
-      <Row>
-        {/* <Carousel autoplay={false} className="sector-carousel">
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-        </Carousel> */}
+    <React.Fragment>
+      <Row className="sectors">
+        <h2 className="title">القطاعات</h2>
+        <p className="desc">
+          نغطي معظم مدن المملكة، ونتوسع يومياً. نعمل في قطاعات ومجالات مختلفة في
+          السوق السعودي، كالضيافة، قطاع التجزئة، القطاع الصحي والمزيد...
+        </p>
       </Row>
-    </Row>
+      <Carousel />
+    </React.Fragment>
   );
 };
 
-export default withTranslation()(JobsTypes);
+export default withTranslation()(Sectors);
