9bf48343c9fc3664b65c4ac11a401489c03ff47b
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-04-26 17:26:23 +0300

    handle registration error

--- src/components/User/login/index.js
+++ src/components/User/login/index.js
@@ -61,7 +61,7 @@ class UserLoginForm extends React.Component {
                   valuePropName: 'checked',
                   initialValue: true
                 })(<Checkbox>تذكرني</Checkbox>)}
-                {user.error && (
+                {user.error && user.error.response && (
                   <Alert message={user.error.response.data} type="error" />
                 )}
               </Form.Item>
