5b927891011cb501335c04b77a63b5a36b21100b
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-05-13 02:33:52 +0300

    update select field options

--- src/components/NewJob/company.js
+++ src/components/NewJob/company.js
@@ -1,29 +1,69 @@
 import React from 'react';
-import { Select, Input, Radio } from 'antd';
+import { Select, Input, Radio, Modal } from 'antd';
 import './style.scss';
 import LoginNavbar from '../Header/LoginNavbar';
 import Footer from '../Footer';
 import TempForm from '../../services/newForm';
 const { getCompanyNewJob } = TempForm;
 export class CompanyNewJob extends React.Component {
+  state = {
+    visible: false,
+    error: false,
+    jobTitle: []
+  };
   handleChange = e => {
     const { value, name } = e.target;
     this.setState({
       [name]: value
     });
   };
+
   handleSelectChange = (value, option) => {
+    const ids = option.map(elm => elm.key);
     this.setState({
-      jobTitle: value
+      jobTitle: ids
     });
   };
 
   send = async e => {
     e.preventDefault();
-    await getCompanyNewJob(this.state);
+    const { jobTitle } = this.state;
+    if (jobTitle.length > 3) {
+      this.setState({
+        error: true
+      });
+    } else {
+      await getCompanyNewJob(this.state);
+      this.setState({
+        visible: true
+      });
+    }
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    });
   };
 
   render() {
+    const jobs = [
+      'Engineering',
+      'Product',
+      'Design',
+      'Data Analytics',
+      'Marketing',
+      'Sales',
+      'Customer Success',
+      'Support',
+      'Operations',
+      'Finance / Accounting',
+      'People / HR / Talent',
+      'Legal',
+      'IT',
+      'Quality Assurance',
+      'Retail / Service'
+    ];
     return (
       <React.Fragment>
         <LoginNavbar />
@@ -35,11 +75,23 @@ export class CompanyNewJob extends React.Component {
             <h4>البريد الإلكتروني:</h4>
             <Input onChange={this.handleChange} name="email" />
             <h4>تأكيد البريد الالكتروني:</h4>
-            <Input onChange={this.handleChange} name="re-email" />
+            <Input onChange={this.handleChange} name="reemail" />
+            {this.state.email !== this.state.reemail &&
+              this.state.email &&
+              this.state.reemail && (
+                <span style={{ color: 'red' }}>
+                  البريد الالكتروني غير متطابق
+                </span>
+              )}
             <h4>رقم الجوال:</h4>
             <Input onChange={this.handleChange} name="mobile" />
             <h4>تأكيد رقم الجوال:</h4>
             <Input onChange={this.handleChange} name="remobile" />
+            {this.state.mobile !== this.state.remobile &&
+              this.state.remobile &&
+              this.state.mobile && (
+                <span style={{ color: 'red' }}>رقم الجوال غير متطابق</span>
+              )}
             <h4>اسم الشركة:</h4>
             <Input onChange={this.handleChange} name="companyName" />
             <h4>
@@ -73,12 +125,24 @@ export class CompanyNewJob extends React.Component {
                 يمكنك اختيار 3 فقط
               </span>
             </h4>
-            <Select className="select" onChange={this.handleSelectChange}>
-              <Select.Option value="xx" name="jobTitle">
-                {' '}
-                xx
-              </Select.Option>
+            <Select
+              className="select"
+              onChange={this.handleSelectChange}
+              mode="multiple"
+              showArrow={true}
+            >
+              {jobs.map(elm => {
+                return (
+                  <Select.Option value={elm} key={elm} name="jobTitle">
+                    {' '}
+                    {elm}
+                  </Select.Option>
+                );
+              })}
             </Select>
+            {this.state.jobTitle && this.state.jobTitle.length > 3 && (
+              <span style={{ color: 'red' }}>يمكنك اختيار 3 خيارات فقط</span>
+            )}
             <h4>عدد سنوات الخبرة؟</h4>
             <Radio.Group
               onChange={this.handleChange}
@@ -104,6 +168,25 @@ export class CompanyNewJob extends React.Component {
           </form>
         </div>
         <Footer />
+        <Modal
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          footer={false}
+        >
+          <div className="success-modal">
+            <h2>تم استلام طلبك، وسيتم التواصل معك في حال إيجاد مرشح مناسب.</h2>
+            <br />
+            <h3 style={{ color: '#049ad0' }}>
+              في حال لم تكن مسجل معنا، سجل الآن!
+            </h3>
+            <br />
+            <button>
+              <a href="https://www.ptway.net/company/signup">
+                <span className="coloor">سجل في PTway</span>
+              </a>
+            </button>
+          </div>
+        </Modal>
       </React.Fragment>
     );
   }
