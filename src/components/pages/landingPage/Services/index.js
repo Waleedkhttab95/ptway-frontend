eda564ae73572d800d2d091b3dc35dd88aa62e94
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-05-31 20:15:51 +0300

    fix tabs bug and style

--- src/components/pages/landingPage/Services/index.js
+++ src/components/pages/landingPage/Services/index.js
@@ -23,28 +23,44 @@ const HowItWorks = () => {
           defaultActiveKey="1"
           animated={false}
           className="works-tab"
-          tabPosition="top"
-          keyboard={true}
+          // tabPosition="top"
           tabBarStyle={{ '-webkit-backface-visibility': ' hidden' }}
-          size="default"
-          tabBarGutter="10"
+          // size="small"
+          // tabBarGutter="5"
         >
-          <TabPane tab="يومي" key="1" forceRender={true}>
-            تستطيع من خلال هذا الخيار الحصول على موظفين للعمل ليوم واحد أو لعدة
-            أيام حسب الطلب، يناسب الشركات المهتمة بتنظيم المعارض والمؤتمرات
-            والإحتفالات والتسويق والترويج
+          <TabPane tab=" تطوعي" key="4" className="custom-tab">
+            <p style={{ display: 'inline-block' }}>
+              مجاناً تستطيع الجهات الخيرية والقطاع غير الربحي استخدام المنصة
+              للحصول على متطوعين ، فاذا كنت تمثل هذه القطاعات أو مايشابهها انظم
+              معنا
+            </p>
             <br />
             <Link to="/user/signup" className="tag-link">
               {'<'} انضم معنا
             </Link>
             <p className="tag-title">مناسب لـ</p>
             <span className="tag">
-              المناسبات &nbsp;#مواسم_السعودية&nbsp; #المعارض# &nbsp; &nbsp;
+              تطوع_صحي &nbsp;#تطوع_خيري&nbsp; #تطوع_اجتماعي# &nbsp;
             </span>
-            <img src={calender} alt="calender" className="tag-icon" />
+            <img src={voluntery} alt="voluntery" className="tag-icon" />
             <br />
           </TabPane>
-          <TabPane tab="جزئي" key="2" forceRender={true}>
+          <TabPane tab=" عقود" key="3">
+            هذا الخيار يساعدك على إيجاد موظفين لعقود مؤقتة أو موسمية قد تمتد
+            لشهر أو عدة أشهر تناسب الشركات التي تحصل على عقود مؤقتة مثل المشاريع
+            أو تعمل اثناء المواسم الطويله مثل الصيف
+            <br />
+            <Link to="/user/signup" className="tag-link">
+              {'<'} انضم معنا
+            </Link>
+            <p className="tag-title">مناسب لـ</p>
+            <span className="tag">
+              وظائف_صيفية &nbsp;#رمضان&nbsp; #مشاريع# &nbsp;
+            </span>
+            <img src={contract} alt="contract" className="tag-icon" />
+            <br />
+          </TabPane>
+          <TabPane tab="جزئي" key="2">
             يساعدك هذا الخيار الحصول على موظفين بدوام جزئي لتغطية أوقات الذروة
             أو حتى تخليص الأعمال بجميع الاوقات لوظائف مسائية و وظائف صباحية
             ووظائف نهاية الأسبوع مثل المطاعم والمقاهي والشركات الناشئة وقطاع
@@ -60,40 +76,20 @@ const HowItWorks = () => {
             <img src={partial} alt="partial" className="tag-icon" />
             <br />
           </TabPane>
-          <TabPane tab=" عقود" key="3" forceRender={true}>
-            هذا الخيار يساعدك على إيجاد موظفين لعقود مؤقتة أو موسمية قد تمتد
-            لشهر أو عدة أشهر تناسب الشركات التي تحصل على عقود مؤقتة مثل المشاريع
-            أو تعمل اثناء المواسم الطويله مثل الصيف
-            <br />
-            <Link to="/user/signup" className="tag-link">
-              {'<'} انضم معنا
-            </Link>
-            <p className="tag-title">مناسب لـ</p>
-            <span className="tag">
-              وظائف_صيفية &nbsp;#رمضان&nbsp; #مشاريع# &nbsp;
-            </span>
-            <img src={contract} alt="contract" className="tag-icon" />
-            <br />
-          </TabPane>
 
-          <TabPane
-            tab=" تطوعي"
-            key="4"
-            className="custom-tab"
-            forceRender={true}
-          >
-            مجاناً تستطيع الجهات الخيرية والقطاع غير الربحي استخدام المنصة
-            للحصول على متطوعين ، فاذا كنت تمثل هذه القطاعات أو مايشابهها انظم
-            معنا
+          <TabPane tab="يومي" key="1">
+            تستطيع من خلال هذا الخيار الحصول على موظفين للعمل ليوم واحد أو لعدة
+            أيام حسب الطلب، يناسب الشركات المهتمة بتنظيم المعارض والمؤتمرات
+            والإحتفالات والتسويق والترويج
             <br />
             <Link to="/user/signup" className="tag-link">
               {'<'} انضم معنا
             </Link>
             <p className="tag-title">مناسب لـ</p>
             <span className="tag">
-              تطوع_صحي &nbsp;#تطوع_خيري&nbsp; #تطوع_اجتماعي# &nbsp;
+              المناسبات &nbsp;#مواسم_السعودية&nbsp; #المعارض# &nbsp; &nbsp;
             </span>
-            <img src={voluntery} alt="voluntery" className="tag-icon" />
+            <img src={calender} alt="calender" className="tag-icon" />
             <br />
           </TabPane>
         </Tabs>
