09c74a4ec8056daaf26e98b373dfa54840707e50
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2020-12-03 12:54:22 +0200

    add header to interview deatials page on mobile

--- src/components/User/interviews/interview.js
+++ src/components/User/interviews/interview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col, Divider, Button, Modal, Result, Checkbox } from 'antd';
 import './index.scss';
 import interviewsData from '../../../services/user/interviews';
+import Header from '../../Header';
 
 const { changeAppointmentStatus, getInterview } = interviewsData;
 export class Interview extends React.Component {
@@ -68,159 +69,162 @@ export class Interview extends React.Component {
 
     const { showSuccessModal, showCancelModal, reason } = this.state;
     return (
-      <Row justify="center" type="flex">
-        <Col span={24} className="interview-information">
-          <h5>استعد للمقابلة الوظيفية</h5>
-          <p>
-            لقد تقدمت على وظيفة: {appointment?.jobAd?.job_Name} في شركة:
-            {appointment?.company?.companyName}
-            <br /> بإمكانك الحضور في هذه الأوقات
-          </p>
-          <Divider />
-          <Row>
-            <span className="interview-days">أيام المقابلات:</span>
-            {appointment?.days.map(elm => (
-              <span className="interview-day" key={elm}>
-                {elm}
-              </span>
-            ))}
-          </Row>
-          <br /> <br />
-          <Row>
-            <Col span={12}>
-              <span className="interview-days">إلى:</span>
-              <span>{appointment?.endDate}</span>
-            </Col>
-            <Col span={12}>
-              <span className="interview-days">تبدأ بتاريخ:</span>
-              <span>{appointment?.startDate}</span>
-            </Col>
-          </Row>
-          <br /> <br />
-          <Row>
-            <Col span={12}>
-              <span className="interview-days">إلى الساعة:</span>
-              <span>{appointment?.endHour}</span>
-            </Col>
-            <Col span={12}>
-              <span className="interview-days">من الساعة:</span>
-              <span>{appointment?.startHour}</span>
-            </Col>
-          </Row>
-          <Divider></Divider>
-          <p>{appointment?.Address}</p>
-          <a className="google-map-link">{appointment?.GoogleMapAddress}</a>
-          <Divider></Divider>
-          <Row>
-            <Col span={12}>
-              <img src={require('../../../images/phone.svg')} />
-              <span>{appointment?.appointmentLeadNumber}</span>
-            </Col>
-            <Col span={12}>
-              <span className="interview-days">مسؤول المقابلات:</span>
-              <span>{appointment?.appointmentLeadName}</span>
-            </Col>
-          </Row>
-          <Row type="flex" justify="center">
-            <Button
-              className="ready interview-btns"
-              onClick={() => this.approveInterview(appointment._id)}
+      <>
+        {window.innerWidth < 768 && <Header />}
+        <Row justify="center" type="flex">
+          <Col span={24} className="interview-information">
+            <h5>استعد للمقابلة الوظيفية</h5>
+            <p>
+              لقد تقدمت على وظيفة: {appointment?.jobAd?.job_Name} في شركة:
+              {appointment?.company?.companyName}
+              <br /> بإمكانك الحضور في هذه الأوقات
+            </p>
+            <Divider />
+            <Row>
+              <span className="interview-days">أيام المقابلات:</span>
+              {appointment?.days.map(elm => (
+                <span className="interview-day" key={elm}>
+                  {elm}
+                </span>
+              ))}
+            </Row>
+            <br /> <br />
+            <Row>
+              <Col span={12}>
+                <span className="interview-days">إلى:</span>
+                <span>{appointment?.endDate}</span>
+              </Col>
+              <Col span={12}>
+                <span className="interview-days">تبدأ بتاريخ:</span>
+                <span>{appointment?.startDate}</span>
+              </Col>
+            </Row>
+            <br /> <br />
+            <Row>
+              <Col span={12}>
+                <span className="interview-days">إلى الساعة:</span>
+                <span>{appointment?.endHour}</span>
+              </Col>
+              <Col span={12}>
+                <span className="interview-days">من الساعة:</span>
+                <span>{appointment?.startHour}</span>
+              </Col>
+            </Row>
+            <Divider></Divider>
+            <p>{appointment?.Address}</p>
+            <a className="google-map-link">{appointment?.GoogleMapAddress}</a>
+            <Divider></Divider>
+            <Row>
+              <Col span={12}>
+                <img src={require('../../../images/phone.svg')} />
+                <span>{appointment?.appointmentLeadNumber}</span>
+              </Col>
+              <Col span={12}>
+                <span className="interview-days">مسؤول المقابلات:</span>
+                <span>{appointment?.appointmentLeadName}</span>
+              </Col>
+            </Row>
+            <Row type="flex" justify="center">
+              <Button
+                className="ready interview-btns"
+                onClick={() => this.approveInterview(appointment._id)}
+              >
+                نعم، أنا جاهز
+              </Button>
+            </Row>
+            <Row type="flex" justify="center">
+              <Button
+                className="cancel interview-btns"
+                onClick={() => this.cancelInterviewModal(appointment._id)}
+              >
+                لست جاهز الآن
+              </Button>
+            </Row>
+            <Modal
+              visible={showSuccessModal}
+              footer={false}
+              className="success-modal"
+              onCancel={this.handleCancel}
             >
-              نعم، أنا جاهز
-            </Button>
-          </Row>
-          <Row type="flex" justify="center">
-            <Button
-              className="cancel interview-btns"
-              onClick={() => this.cancelInterviewModal(appointment._id)}
+              <Result
+                status="success"
+                title="كل التوفيق في المقابلة"
+                subTitle="أظهر حماسك وتمتع بالثقة والتزم بالموعد"
+                extra={
+                  <>
+                    <Row type="flex" justify="center">
+                      <Button
+                        className="ready interview-btns"
+                        onClick={this.handleCancel}
+                      >
+                        العودة لصفحة العروض
+                      </Button>
+                    </Row>
+                    <Row type="flex" justify="center">
+                      <Button
+                        className="cancel interview-btns"
+                        onClick={this.handleCancel}
+                      >
+                        راجع تفاصيل الموعد
+                      </Button>
+                    </Row>
+                  </>
+                }
+              />
+            </Modal>
+            <Modal
+              visible={showCancelModal}
+              footer={false}
+              onCancel={this.handleCancel}
             >
-              لست جاهز الآن
-            </Button>
-          </Row>
-          <Modal
-            visible={showSuccessModal}
-            footer={false}
-            className="success-modal"
-            onCancel={this.handleCancel}
-          >
-            <Result
-              status="success"
-              title="كل التوفيق في المقابلة"
-              subTitle="أظهر حماسك وتمتع بالثقة والتزم بالموعد"
-              extra={
-                <>
-                  <Row type="flex" justify="center">
-                    <Button
-                      className="ready interview-btns"
-                      onClick={this.handleCancel}
-                    >
-                      العودة لصفحة العروض
-                    </Button>
-                  </Row>
-                  <Row type="flex" justify="center">
-                    <Button
-                      className="cancel interview-btns"
-                      onClick={this.handleCancel}
-                    >
-                      راجع تفاصيل الموعد
-                    </Button>
-                  </Row>
-                </>
-              }
-            />
-          </Modal>
-          <Modal
-            visible={showCancelModal}
-            footer={false}
-            onCancel={this.handleCancel}
-          >
-            <div className="cancel-interview-modal">
-              <h3>يهمنا رأيك، لماذا لست جاهز للمقابلة؟</h3>
-              <Checkbox
-                value="أوقات المقابلات غير مناسب"
-                onChange={this.onCancelReasonChange}
-              >
-                أوقات المقابلات غير مناسب
-              </Checkbox>
-              <br />
-              <Checkbox
-                value="لم أعد متهم بهذه الوظيفة"
-                onChange={this.onCancelReasonChange}
-              >
-                لم أعد متهم بهذه الوظيفة
-              </Checkbox>
-              <br />
-              <Checkbox
-                value=" لست متفرغ للوظيفة حالياً"
-                onChange={this.onCancelReasonChange}
-              >
-                لست متفرغ للوظيفة حالياً
-              </Checkbox>
-              <br />
-              <Checkbox
-                value="موقع المقابلة بعيد جداً"
-                onChange={this.onCancelReasonChange}
-              >
-                موقع المقابلة بعيد جداً
-              </Checkbox>
-              <br />
-              <Checkbox value="آخر" onChange={this.onCancelReasonChange}>
-                آخر
-              </Checkbox>
-              <br />
-              <Row type="flex" justify="center">
-                <Button
-                  className={reason ? 'send-btn btn' : 'btn'}
-                  disabled={!reason ? true : false}
-                  onClick={this.cancelInterview}
+              <div className="cancel-interview-modal">
+                <h3>يهمنا رأيك، لماذا لست جاهز للمقابلة؟</h3>
+                <Checkbox
+                  value="أوقات المقابلات غير مناسب"
+                  onChange={this.onCancelReasonChange}
                 >
-                  أنشر
-                </Button>
-              </Row>
-            </div>
-          </Modal>
-        </Col>
-      </Row>
+                  أوقات المقابلات غير مناسب
+                </Checkbox>
+                <br />
+                <Checkbox
+                  value="لم أعد متهم بهذه الوظيفة"
+                  onChange={this.onCancelReasonChange}
+                >
+                  لم أعد متهم بهذه الوظيفة
+                </Checkbox>
+                <br />
+                <Checkbox
+                  value=" لست متفرغ للوظيفة حالياً"
+                  onChange={this.onCancelReasonChange}
+                >
+                  لست متفرغ للوظيفة حالياً
+                </Checkbox>
+                <br />
+                <Checkbox
+                  value="موقع المقابلة بعيد جداً"
+                  onChange={this.onCancelReasonChange}
+                >
+                  موقع المقابلة بعيد جداً
+                </Checkbox>
+                <br />
+                <Checkbox value="آخر" onChange={this.onCancelReasonChange}>
+                  آخر
+                </Checkbox>
+                <br />
+                <Row type="flex" justify="center">
+                  <Button
+                    className={reason ? 'send-btn btn' : 'btn'}
+                    disabled={!reason ? true : false}
+                    onClick={this.cancelInterview}
+                  >
+                    أنشر
+                  </Button>
+                </Row>
+              </div>
+            </Modal>
+          </Col>
+        </Row>
+      </>
     );
   }
 }
